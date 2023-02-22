7431e64b42a5b2cd72e050b3c9cadaf1c320e809
Onee_Only <kimww0306@gmail.com>
2023-02-22 18:19:06 +0900

    Public User 및 Private User serializer 생성

--- users/serializers.py
+++ users/serializers.py
@@ -4,26 +4,47 @@ from .models import User
 
 
 class ListUserSerializer(serializers.ModelSerializer):
-    wallet = WalletSerializer()
+    public_key = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = (
             "pk",
             "username",
-            "wallet",
+            "public_key",
         )
 
 
 class PublicUserSerializer(serializers.ModelSerializer):
-    wallet = WalletSerializer()
+    public_key = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = (
             "pk",
             "username",
-            "wallet",
+            "public_key",
             "avatar",
         )
 
+    def get_public_key(self, user):
+        return user.wallet.public_key
+
+
+class PrivateUserSerializer(serializers.ModelSerializer):
+    wallet = WalletSerializer()
+    message_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            "pk",
+            "email",
+            "username",
+            "wallet",
+            "avatar",
+            "message_count",
+        )
+
+    def get_message_count(self, user):
+        return user.messages.count()
--- wallets/serializers.py
+++ wallets/serializers.py
@@ -5,4 +5,7 @@ from .models import Wallet
 class WalletSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wallet
-        fields = ("public_key",)
+        fields = (
+            "public_key",
+            "private_key_hash",
+        )
