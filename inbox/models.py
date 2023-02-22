3eec9e1739bc1259317baf2600905629753ddd10
Onee_Only <kimww0306@gmail.com>
2023-02-22 12:04:44 +0900

    inbox model 추가

--- inbox/models.py
+++ inbox/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    class MessageType(models.TextChoices):
+        TRANSACTION = "transaction", "Transaction"
+        SYSTEM = "system", "System"
+
+    receiver = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    sender = models.ForeignKey("users.User", on_delete=models.CASCADE, null=True)
+    content = models.CharField(max_length=50)
+    message_type = models.CharField(max_length=20, choices=MessageType.choices)
