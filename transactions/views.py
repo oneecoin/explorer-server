787505ac202d98276dfe37286e2dda7b8d56a3da
Onee_Only <kimww0306@gmail.com>
2023-02-23 11:06:29 +0900

    transaction view 및 model 정의

--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,28 @@
+import datetime
+from django.db.models import Q
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import TransactionSerializer
+from .models import Transaction
 
 
 class TransactionsCount(APIView):
     def get(self, request):
         """see transactions count"""
-        pass
+        scope = request.GET.get("scope")
+        now = datetime.datetime.now().date()
+        if scope == "week":
+            # get last 7 days
+            transactions = Transaction.objects.filter(
+                Q(date__lte=now) & Q(date_gte=now - datetime.timedelta(days=7))
+            )
+        elif scope == "month":
+            # get last 30 days
+            transactions = Transaction.objects.filter(
+                Q(date__lte=now) & Q(date_gte=now - datetime.timedelta(days=30))
+            )
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
